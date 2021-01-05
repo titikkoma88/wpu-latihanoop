c9ea24eb35eac2856cca654c03027845f60441ab
titikkoma88 <jie.koma88@gmail.com>
2021-01-05 20:33:09 +0700

    Initial commit

---
+++ produk.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Produk{
+
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit ="penerbit",
+           $harga = 0;
+    
+}
+
+$produk1 = new Produk();
+
+var_dump($produk1);
